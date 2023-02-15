a0428e31f67ea812e568f529eed24cfb9bf8d69d
David <isaacsdavid28@gmail.com>
2023-02-14 22:26:18 -0500

    added the image and venue name to EventItem

--- src/components/EventItem.tsx
+++ src/components/EventItem.tsx
@@ -8,15 +8,17 @@ export interface IEventItemProps {
 export function EventItem (props: IEventItemProps) {
     let {affair} = props
 
+    const venue = affair._embedded.venues[0]
 
   return (
     <div className='EventItem'>
         <ul>
+            <li>{affair.images[0] && <img src={affair.images[0].url} alt="Event" />}</li>
             <li>{affair.name}</li>
+            <li>{affair.dates.start.localDate}</li>
             <li>{affair.url}</li>
-            <li>{affair.dates.start.dateTBA}</li>
+            <li>{venue && venue.name}</li>
         </ul>
-      
     </div>
   );
 }
