694266f53b597676fa8e575befc949014665de41
Jakob <jakobsenyko@gmail.com>
2023-02-14 20:56:47 -0500

    vdaypush

---
+++ src/services/EventService.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { Events } from "../models/Events";
+
+
+export function GetEventData() :Promise<Events> {
+    return axios.get<Events>("https://app.ticketmaster.com/discovery/v2/events.json?size=5&apikey=LG8mjVMbvHfPJKBLUIbBnzPxZ3V2q8ru")
+    .then(response => response.data)
+}
