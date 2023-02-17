48f08bce8afbc8eb7c558f057320b710d9283a2a
Jakob <jakobsenyko@gmail.com>
2023-02-16 21:37:54 -0500

    Jakobpush

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,8 +1,31 @@
 import axios from "axios";
-import { Events } from "../models/Events";
+import { stringify } from "querystring";
+import { Affair, Events } from "../models/Events";
 
 
 export function GetEventData() :Promise<Events> {
     return axios.get<Events>("https://app.ticketmaster.com/discovery/v2/events.json?size=5&apikey=LG8mjVMbvHfPJKBLUIbBnzPxZ3V2q8ru")
     .then(response => response.data)
+}
+
+export function queryEvent(keyword:string, city:string, stateCode:string) {
+
+    
+    return axios.get<Events>("https://app.ticketmaster.com/discovery/v2/events.json", {
+        params:{
+            apikey:"LG8mjVMbvHfPJKBLUIbBnzPxZ3V2q8ru",
+            Keyword: keyword || "",
+            City:city || "",
+            stateCode:stateCode || ""
+        }
+    })
+    .then(response => response.data)
+        //handle successful response, extract relevant data from the API response
+        //const events:Affair[] = response.data?._embedded?.events || [];
+        
+    .catch((error) =>{
+        console.log(error)
+    })
+
+    
 }
