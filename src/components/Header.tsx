e0d1dbf633e586ab977d01c357eda531cd3a3c3c
Tania Gherasim <taniaursan@gmail.com>
2023-02-16 20:20:47 -0500

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ export function Header (props: IHeaderProps) {
   return (
     <div className="Header">
       <h1>Events</h1>
+      <h3>Find Your Next Event!</h3>
       <p id="BucketList">BucketList</p>
     </div>
   );
